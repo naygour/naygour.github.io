883964b459a672db7caf6aa015c79b6297b934be
Bruwaert Theo <bruwaert@myserver.org>
2022-04-18 19:57:21 +0200

    Ajout d'un projet et modif du carrousel

--- script.js
+++ script.js
@@ -53,19 +53,14 @@ loop();
 
 //================================================ Carrousel Swiper =====================================
 
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+    var swiper = new Swiper(".mySwiper", {
+      effect: "flip",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+        },
+    navigation: {
+      nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+        },
+      });
